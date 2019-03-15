46cf4358dccfd87c38e2c4f2230a779b7a89d610
macibor811@student.polsl.pl <macibor811@student.polsl.pl>
2019-03-15 13:56:38 +0100

    REST

--- RepairManagment/src/main/java/pl/polsl/repairmanagment/entities/AddressRepository.java
+++ RepairManagment/src/main/java/pl/polsl/repairmanagment/entities/AddressRepository.java
@@ -1,24 +1,41 @@
 package pl.polsl.repairmanagment.entities;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.persistence.EntityManager;
+import java.util.List;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 
-@Repository
-public class AddressRepository {
+@RepositoryRestResource(collectionResourceRel = "addresses", path = "address")
+public interface AddressRepository extends PagingAndSortingRepository<AddressEntity, Integer> {
 
-    @Autowired
-    private final EntityManager entityManager;
+   // @Autowired
+   // public final EntityManager entityManager;
 
-    public AddressRepository(EntityManager entityManager) {
-        this.entityManager = entityManager;
-    }
+//    public AddressRepository(EntityManager entityManager) {
+//        this.entityManager = entityManager;
+//    }
+//
+//    @Transactional
+//    public void save(AddressEntity address) {
+//        entityManager.persist(address);
+//    }
 
-    @Transactional
-    public void save(AddressEntity address) {
-        entityManager.persist(address);
-    }
+    List<AddressEntity> findAll();
+
+    @GetMapping("/byid")
+    @ResponseBody
+    List<AddressEntity> findAllById(@RequestParam Integer id);
 }
--- RepairManagment/src/main/resources/schema.sql
+++ RepairManagment/src/main/resources/schema.sql
@@ -1,7 +1,7 @@
 CREATE TABLE client (
                        id INTEGER PRIMARY KEY NOT NULL,
-                       first_name VARCHAR(255),
-                       last_name VARCHAR(255),
+                       first_name VARCHAR(50),
+                       last_name VARCHAR(50),
                        phone_number VARCHAR(10),
                        address_id INTEGER REFERENCES address(id) NOT NULL
 );
@@ -17,7 +17,7 @@ CREATE TABLE address (
 
 CREATE TABLE item_type (
                     id INTEGER PRIMARY KEY NOT NULL,
-                    type VARCHAR(255)
+                    type VARCHAR(50)
 );
 
 CREATE TABLE item (
@@ -26,3 +26,51 @@ CREATE TABLE item (
                        item_type_id INTEGER REFERENCES item_type(id) NOT NULL,
                        owner_id INTEGER REFERENCES client(id) NOT NULL
 );
+
+CREATE TABLE personnel (
+                      id INTEGER PRIMARY KEY NOT NULL,
+                      first_name VARCHAR(50),
+                      last_name VARCHAR(50),
+                      phone_number VARCHAR(10),
+                      address_id INTEGER REFERENCES address(id) NOT NULL,
+                      role VARCHAR(10),
+                      username VARCHAR(50),
+                      password VARCHAR(50)
+);
+
+
+CREATE TABLE request (
+                    id INTEGER PRIMARY KEY NOT NULL,
+                    description VARCHAR(1024),
+                    result VARCHAR(3),
+                    status VARCHAR(3),
+                    register_date TIMESTAMP,
+                    end_date TIMESTAMP,
+
+                    item_id INTEGER REFERENCES item(id) NOT NULL,
+                    manager_id INTEGER REFERENCES personnel(id) NOT NULL
+);
+
+CREATE TABLE activity_type (
+                         id INTEGER PRIMARY KEY NOT NULL,
+                         type VARCHAR(50)
+);
+
+CREATE TABLE activity (
+                       id INTEGER PRIMARY KEY NOT NULL,
+                       sequence_num INTEGER,
+                       description VARCHAR(1024),
+                       result VARCHAR(3),
+                       status VARCHAR(3),
+                       register_date TIMESTAMP,
+                       end_date TIMESTAMP,
+
+                      request_id INTEGER REFERENCES request(id) NOT NULL,
+                      worker_id INTEGER REFERENCES personnel(id) NOT NULL
+
+);
+
+
+
+
+
